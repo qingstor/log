f0b983139c0c67912cc0fbd2cda4c4bf4d1b9642
Lance Ren <lanceren@yunify.com>
2020-08-04 15:48:23 +0800

    Apply review message

--- context.go
+++ context.go
@@ -14,18 +14,23 @@ func ContextWithLogger(ctx context.Context, l *Logger) context.Context {
 	if ctx == nil {
 		ctx = context.Background()
 	}
+	// if nil logger was given, return ctx directly
+	if l == nil {
+		return ctx
+	}
+
 	return context.WithValue(ctx, loggerKey, l)
 }
 
 // FromContext get *Logger from context
-// Notice: If ctx is nil or no Logger was set before, it will return nil
+// Notice: If ctx is nil or no Logger was set before, it will return a default logger
 func FromContext(ctx context.Context) *Logger {
 	if ctx == nil {
-		return nil
+		return New()
 	}
 	l, ok := ctx.Value(loggerKey).(*Logger)
 	if !ok {
-		return nil
+		return New()
 	}
 	return l
 }
--- context_test.go
+++ context_test.go
@@ -36,39 +36,61 @@ func TestContextWithLogger(t *testing.T) {
 			if !reflect.DeepEqual(tt.l, logger) {
 				t.Errorf("want %v, got %v", tt.l, logger)
 			}
+
+			// set nil logger one more time
+			got = ContextWithLogger(got, nil)
+			logger, ok = got.Value(loggerKey).(*Logger)
+			if !ok {
+				t.Errorf("not validated logger")
+			}
+			if !reflect.DeepEqual(tt.l, logger) {
+				t.Errorf("want %v, got %v", tt.l, logger)
+			}
 		})
 	}
 }
 
 func TestFromContext(t *testing.T) {
+	defaultLogger := New()
 	l := &Logger{}
 	ctxWithLogger := ContextWithLogger(nil, l)
 	tests := []struct {
-		name string
-		ctx  context.Context
-		want *Logger
+		name        string
+		ctx         context.Context
+		loggerExist bool
+		want        *Logger
 	}{
 		{
-			name: "nil ctx",
-			ctx:  nil,
-			want: nil,
+			name:        "nil ctx",
+			ctx:         nil,
+			loggerExist: false,
+			want:        defaultLogger,
 		},
 		{
-			name: "not set",
-			ctx:  context.Background(),
-			want: nil,
+			name:        "not set",
+			ctx:         context.Background(),
+			loggerExist: false,
+			want:        defaultLogger,
 		},
 		{
-			name: "logger set",
-			ctx:  ctxWithLogger,
-			want: l,
+			name:        "logger set",
+			ctx:         ctxWithLogger,
+			loggerExist: true,
+			want:        l,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := FromContext(tt.ctx)
-			if !reflect.DeepEqual(tt.want, got) {
-				t.Errorf("want %v, got %v", tt.want, got)
+			if tt.loggerExist {
+				if !reflect.DeepEqual(tt.want, got) {
+					t.Errorf("want logger %v, got %v", tt.want, got)
+				}
+			} else {
+				tfArg, tfRes := tt.want.transformer.(*Text), got.transformer.(*Text)
+				if !reflect.DeepEqual(tfArg.vc, tfRes.vc) {
+					t.Errorf("want tf %#v, got %#v", tfArg, tfRes)
+				}
 			}
 		})
 	}
